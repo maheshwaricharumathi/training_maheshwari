8b684c9e31a718567f0370478ea500c9ff30a575
maheshwaricharumathi <32409216+maheshwaricharumathi@users.noreply.github.com>
2017-12-18 13:10:30 +0530

    Create carfactory.h

---
+++ carFactory/carfactory.h
@@ -0,0 +1,23 @@
+#include "carfactory.h"
+#include"car.h"
+
+void main()
+{
+		car suzuki;
+		car volvo;
+		car pmw;
+		car mars;
+    car skoda;
+    suzuki.get_details();
+		suzuki.print_details();
+    volvo.get_details();
+		volvo.print_details();
+    pmw.get_details();
+		pmw.print_details();
+    mars.get_details();
+		mars.print_details();
+    skoda.get_details();
+		skoda.print_details();
+		system("pause");
+
+}
