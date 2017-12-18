e1295b698e07c3b6843690d995967cb995552463
maheshwaricharumathi <32409216+maheshwaricharumathi@users.noreply.github.com>
2017-12-18 13:14:55 +0530

    Create car.cpp

---
+++ carFactory/parameterized_constructor/car.cpp
@@ -0,0 +1,34 @@
+#include "car.h"
+car::car(string make_model, string fuel_type, int s_no, string mod)
+{
+	MakeModel = make_model;
+	FuelType = fuel_type;
+	SerialNo = s_no;
+	model = mod;
+}
+void car::get_details()
+
+{
+cout << "Enter the make model" << endl;
+cin >> MakeModel;
+cout << "Enter the fuel_type" << endl;
+cin >> FuelType;
+cout << "Enter the serialno" << endl;
+cin >> SerialNo;
+cout << "Entre the model" << endl;
+cin >> model;
+
+}
+void car::print_details()
+{
+	cout << "The MakeModel is" << endl;
+	cout << MakeModel << endl;
+	cout << "The fueltype is" << endl;
+	cout << FuelType << endl;
+	cout << "The serialNo is" << endl;
+	cout << SerialNo << endl;
+	cout << "The model is" << endl;
+	cout << model << endl;
+}
+	
+ 
