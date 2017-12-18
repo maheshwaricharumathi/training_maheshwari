2f903ae0c4312d1f13cf12f8398ead1651cdb676
maheshwaricharumathi <32409216+maheshwaricharumathi@users.noreply.github.com>
2017-12-18 13:06:10 +0530

    Create car.h

---
+++ carFactory/car.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<string>
+#include<iostream>
+using namespace std;
+class car
+{
+public:
+	string MakeModel;
+	string FuelType;
+	int SerialNo;
+	string model;
+	public:
+		void get_details();
+	void print_details();
+};
+
+　
+	
