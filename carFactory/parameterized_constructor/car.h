1a788890506d0f49af555af0828ee948933b6222
maheshwaricharumathi <32409216+maheshwaricharumathi@users.noreply.github.com>
2017-12-18 13:14:33 +0530

    Create car.h

---
+++ carFactory/parameterized_constructor/car.h
@@ -0,0 +1,19 @@
+#pragma once
+#include<string>
+#include<iostream>
+using namespace std;
+class car
+{
+public:
+	string MakeModel;
+	string FuelType;
+	int SerialNo;
+	string model;
+	public:
+	car(string, string, int, string);
+	void get_details();
+	void print_details();
+};
+
+　
+	
