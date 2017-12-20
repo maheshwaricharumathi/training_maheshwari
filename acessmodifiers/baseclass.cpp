775f0891114f8e4ff15f9edb3dc7e266ae4f4f4d
maheshwaricharumathi <32409216+maheshwaricharumathi@users.noreply.github.com>
2017-12-20 11:36:49 +0530

    Update baseclass.cpp

--- acessmodifiers/baseclass.cpp
+++ acessmodifiers/baseclass.cpp
@@ -11,13 +11,14 @@ void  main()
 	derived derivedobj;
 	int actual_variable1 = 100;
 	int actual_variable2 = 50, actual_variable3 = 90;
-	derivedobj.r1();
 	paramconst.printfunction();
 	baseobj.publicfunction(actual_variable1);
-	baseobj.privatecallfunction();
+	baseobj.calling_function();
 	baseobj.publicfunction(&actual_variable2, &actual_variable3);
 	baseobj.publicfunction();
-	derivedobj.publicfunction();
+	derivedobj.overriding_function();
+	derivedobj.derivedprotected();
+	derivedobj.derivedprivate();
 	system("pause");
 
 }
