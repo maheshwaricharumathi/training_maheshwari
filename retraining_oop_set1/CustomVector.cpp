a8e7825127a391c773404aed0b768600be4685a1
maheshwaricharumathi <32409216+maheshwaricharumathi@users.noreply.github.com>
2017-12-29 18:09:57 +0530

    Create CustomVector.cpp

---
+++ retraining_oop_set1/CustomVector.cpp
@@ -0,0 +1,10 @@
+#include "CustomVector.h"
+void main()
+{
+	CustomVector(vector<int>(1));
+	CustomVector(vector<int>(5),vector<double>(1));
+	CustomVector paracust(10);
+	CustomVector copyconst = paracust;
+	system("pause");
+
+}
